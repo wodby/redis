776239b01c5478342cb46d154d2c721ad05613d4
Chingis <csandanov@users.noreply.github.com>
2017-04-14 17:45:45 +0700

    Update docker-entrypoint.sh

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -10,7 +10,7 @@ gotpl "/etc/gotpl/redis.conf.tpl" > "/etc/redis.conf"
 sed -i '/^\s*$/d' /etc/redis.conf
 
 if [[ $1 == "make" ]]; then
-    exec /usr/local/bin/docker-entrypoint.sh "${@}" -f /usr/local/bin/actions.mk
+    exec "${@}" -f /usr/local/bin/actions.mk
 else
     exec /usr/local/bin/docker-entrypoint.sh "${@}"
 fi
