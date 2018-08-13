d329e45ca32e087342cb55ec36c3e2c7af065022
Chingis Sandanov <chingis@wodby.com>
2018-08-13 18:43:10 +0700

    Minor fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ chown redis:redis /data
 gotpl /etc/gotpl/redis.conf.tmpl > /etc/redis.conf
 sed -i '/^\s*$/d' /etc/redis.conf
 
-if [[ $1 == "make" ]]; then
+if [[ "${1}" == "make" ]]; then
     exec "${@}" -f /usr/local/bin/actions.mk
 else
     exec /usr/local/bin/docker-entrypoint.sh "${@}"
