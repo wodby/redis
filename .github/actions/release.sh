c75830ec5b0292b4d9a9f1cada4034bec5c427f4
Chingis Sandanov <chingis@chingis.io>
2025-11-20 13:58:52 +0800

    Fix branch

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-if [[ "${GITHUB_REF}" == refs/heads/main || "${GITHUB_REF}" == refs/tags/* ]]; then      
+if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]]; then      
   minor_ver="${REDIS_VER%.*}"
   major_ver="${minor_ver%.*}"
 
